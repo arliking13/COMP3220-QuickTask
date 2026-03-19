fd271adb5e00f56e78b007c4af5cacca1165d943
SedaiUwU <VanshS512004@gmail.com>
2026-03-19 19:22:19 -0400

    Test- vansh

--- randtest.java
+++ randtest.java
@@ -1,5 +1,6 @@
-public class MyClass {
+public class randtest {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
+        System.out.println("Vansh");
     }
 }

a814c0ea5f0fda89082c3ca73215f93f97ed3e1f
SedaiUwU <VanshS512004@gmail.com>
2026-03-19 19:52:03 -0400

    Cleaned up

--- randtest.java
+++
@@ -1,8 +0,0 @@
-public class randtest {
-    public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
-        System.out.println("Vansh");
-        System.out.println("Temirlan");
-        yooooo
-    }
-}
--- src/randtest.java
+++
@@ -1,6 +0,0 @@
-public class randtest {
-    public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
-        System.out.println("Vansh");
-    }
-}
